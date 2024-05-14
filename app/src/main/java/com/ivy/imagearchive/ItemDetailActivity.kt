afbfe07a2791bb8b9d00838ee7153853dc289875
Jiwoo Lim <dlawldn10@gmail.com>
2024-05-14 20:00:42 +0900

    Style: Add favorite screen layout & item detail screen layout

---
+++ app/src/main/java/com/ivy/imagearchive/ItemDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.ivy.imagearchive
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.tabs.TabLayout
+import com.ivy.imagearchive.databinding.ActivityMainBinding
+
+class ItemDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/ivy/imagearchive/ui/itemdetail/ItemDetailFragment.kt
@@ -0,0 +1,36 @@
+package com.ivy.imagearchive.ui.itemdetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.ivy.imagearchive.databinding.FragmentItemDetailBinding
+import com.ivy.imagearchive.ui.favorite.FavoriteViewModel
+
+class ItemDetailFragment : Fragment() {
+
+    private var _binding: FragmentItemDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
